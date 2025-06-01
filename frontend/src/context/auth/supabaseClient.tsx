8cac9b181779792e582d3b93b94e5246e81bda3c
ashcodex505 <98429783+ashcodex505@users.noreply.github.com>
2025-06-01 11:42:30 -0700

    Update supabaseClient.tsx

--- frontend/src/context/auth/supabaseClient.tsx
+++ frontend/src/context/auth/supabaseClient.tsx
@@ -3,20 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Get the site URL based on environment
-const getSiteUrl = () => {
-  if (import.meta.env.VITE_ENV_STATE === "development") {
-    return "http://localhost:5173";
-  }
-  return "https://tamid-tech-blog-challenge.vercel.app";
-};
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     storage: localStorage,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
-    flowType: 'pkce'
+    detectSessionInUrl: true
   }
 });
